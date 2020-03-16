c6411746f09a8b44b2d5416388c7f67219b421e1
Kevin Moore <moore.kevin.p@gmail.com>
2020-03-16 15:39:42 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const MOVIEDEX = require('./moviedex.json');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
+
+app.get('/movie', function handleGetMovies(req, res) {
+  //start here
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
